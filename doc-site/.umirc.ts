6e87181656a6bbd0ccdf12a0fbecf1a0d88db452
sunpanyu <996251389@qq.com>
2022-09-05 16:08:42 +0800

    feat: add docs.

--- doc-site/.umirc.ts
+++ doc-site/.umirc.ts
@@ -20,6 +20,7 @@ export default defineConfig({
   ],
   dynamicImport: {},
   publicPath: process.env.NODE_ENV === 'production' ? '/react-antd-library/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/react-antd-library/' : '/',
   navs: [
     null, // null 值代表保留约定式生成的导航，只做增量配置
     {
