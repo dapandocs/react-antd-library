38855ad53f06a4b46f935c2f8226f6253815a177
react-spy <996251389@qq.com>
2022-09-24 19:35:28 +0800

    ci(EditableTable): improve functions.

--- examples/umijs/src/pages/table/index.tsx
+++ examples/umijs/src/pages/table/index.tsx
@@ -1,57 +1,141 @@
 import React from "react";
+import { Form, Button, Table } from "antd";
+import { useUpdate, useSetState } from "ahooks";
 import { EditableTable, EditableTableColumns } from "../../../../../src";
 const dataSource = [
   {
     key: "1",
-    name: "胡彦斌",
+    name: "zhangsan",
     age: 32,
     address: "西湖区湖底公园1号",
   },
   {
     key: "2",
-    name: "",
+    name: "lisi",
     age: 42,
     address: "西湖区湖底公园1号",
   },
 ];
 
-const columns: EditableTableColumns<any>[] = [
-  {
-    title: "姓名",
-    dataIndex: "name",
-    key: "name",
-    valueType: "select",
-    antdComponentProps: {
-      select: {
-        allowClear:false,
-        options: [
-          {
-            label: "张三",
-            value: "zhangsan",
-          },
-          {
-            label: "李四",
-            value: "lisi",
-          },
-        ],
+const Editable = () => {
+  const [form] = Form.useForm();
+  const update = useUpdate();
+  const [state, setState] = useSetState<any>({
+    isedit: false,
+    data: dataSource,
+  });
+  const { isedit, data } = state;
+  const columns: EditableTableColumns<any>[] = [
+    {
+      title: "ID",
+      dataIndex: "key",
+      isHidden: true,
+      key: "key",
+    },
+    {
+      title: "姓名",
+      dataIndex: "name",
+      key: "name",
+      valueType: "select",
+      formItemProps: {
+        rules: [{ message: "请输入", required: true }],
+      },
+      antdComponentProps: {
+        select: {
+          // allowClear: false,
+          options: [
+            {
+              label: "张三",
+              value: "zhangsan",
+            },
+            {
+              label: "李四",
+              value: "lisi",
+            },
+          ],
+        },
       },
     },
-  },
-  {
-    title: "年龄",
-    dataIndex: "age",
-    key: "age",
-    valueType: "inputNumber",
-  },
-  {
-    title: "住址",
-    dataIndex: "address",
-    key: "address",
-  },
-];
-const Editable = () => {
+    {
+      title: "年龄",
+      dataIndex: "age",
+      key: "age",
+      valueType: "inputNumber",
+    },
+    {
+      title: "住址",
+      dataIndex: "address",
+      key: "address",
+      isEditable: isedit,
+      // isHidden: true,
+    },
+    {
+      title: "时间",
+      dataIndex: "time",
+      valueType: "datePicker",
+      isEditable: isedit,
+    },
+    {
+      title: "操作",
+      valueType: "option",
+    },
+  ];
   return (
-    <EditableTable rowKey="key" columns={columns} dataSource={dataSource} />
+    <>
+      <EditableTable form={form} columns={columns} dataSource={data} />
+      <Button
+        type="primary"
+        onClick={() => {
+          const values = form.getFieldsValue();
+          console.log(values);
+        }}
+      >
+        打印
+      </Button>
+      <Button
+        type="primary"
+        onClick={() => {
+          update();
+        }}
+      >
+        强制更新
+      </Button>
+      <Button
+        onClick={() => {
+          setState({ isedit: !isedit });
+        }}
+      >
+        切换
+      </Button>
+      <Button
+        onClick={() => {
+          setState({
+            data: [
+              {
+                key: "1",
+                name: "zhangsan",
+                age: 32,
+                address: "西湖区湖底公园1号",
+              },
+              {
+                key: "2",
+                name: "lisi",
+                age: 42,
+                address: "西湖区湖底公园1号",
+              },
+              {
+                key: "3",
+                name: "lisi",
+                age: 22,
+                address: "西湖区湖底公园1号",
+              },
+            ],
+          });
+        }}
+      >
+        重置
+      </Button>
+    </>
   );
 };
 export default Editable;
--- src/components/AntdTable/EditableTable.tsx
+++ src/components/AntdTable/EditableTable.tsx
@@ -1,48 +1,100 @@
 import React from "react";
-import { Table, Form } from "antd";
+import { Table, Form, Button, Tooltip } from "antd";
+import { DeleteOutlined } from "@ant-design/icons";
 import type {
   TableColumnProps,
   TableProps,
   InputProps,
   SelectProps,
   InputNumberProps,
+  FormInstance,
+  FormItemProps,
+  DatePickerProps,
 } from "antd";
+import { useDynamicList, useDeepCompareEffect } from "ahooks";
 import { PreviewText } from "../PreviewText";
 import styles from "./EditableTable.less";
 
 export interface EditableTableColumns<RecordType>
   extends TableColumnProps<RecordType> {
-  valueType?: "input" | "select" | "inputNumber";
+  valueType?: "input" | "select" | "inputNumber" | "datePicker" | "option";
   antdComponentProps?: {
     input?: InputProps;
     select?: SelectProps;
     inputNumber?: InputNumberProps;
+    datePicker?: DatePickerProps;
   };
+  formItemProps?:
+    | ((
+        form?: FormInstance,
+        config?: {
+          rowIndex?: number;
+          colIndex?: number;
+          name?: string[];
+          dataIndex?: string;
+          listName?: string;
+        }
+      ) => FormItemProps)
+    | FormItemProps;
+  isEditable?: boolean;
+  isHidden?: boolean;
+  render?: (
+    text?: string,
+    row?: any,
+    index?: any,
+    action?: {
+      remove?: (index: number) => void;
+      insert?: (index: number, item: any) => void;
+      replace?: (index: number, item: any) => void;
+    }
+  ) => React.ReactNode | null;
 }
 
 export interface EditableTableProps extends TableProps<any> {
   columns?: EditableTableColumns<any>[];
+  dataSource?: Record<string, any>[];
+  showAddButton?: boolean;
+  form?: FormInstance;
+  listName?: string;
 }
 
-const { Input, Select, InputNumber } = PreviewText;
+const { Input, Select, InputNumber, DatePicker } = PreviewText;
 
 const valueTypeMap: Record<string, any> = {
   input: Input,
   select: Select,
   inputNumber: InputNumber,
+  datePicker: DatePicker,
 };
 export const EditableTable: React.FC<EditableTableProps> = ({
   columns,
+  form,
+  listName = "list",
+  dataSource = [],
+  showAddButton = true,
   ...restProps
 }) => {
+  const { list, remove, push, resetList, insert, replace, getKey } =
+    useDynamicList<any>([]);
+
+  useDeepCompareEffect(() => {
+    resetList(dataSource);
+  }, [dataSource]);
+
   const getColumns = React.useMemo(() => {
     if (!Array.isArray(columns) || columns.length === 0) {
       return [];
     }
     const columnsResult: EditableTableColumns<any>[] = [];
-    columns.forEach((item: Record<string, any>) => {
+    columns.forEach((item: Record<string, any>, colIndex: number) => {
       let AntdComponent = Input;
-      const { valueType, antdComponentProps = {} } = item;
+      const {
+        valueType,
+        antdComponentProps = {},
+        formItemProps,
+        isEditable = true,
+        isHidden = false,
+      } = item;
       const antdProps = antdComponentProps[valueType]
         ? antdComponentProps[valueType]
         : {};
@@ -59,36 +111,91 @@ export const EditableTable: React.FC<EditableTableProps> = ({
       ) {
         Object.assign(antdProps, { allowClear: true });
       }
-      columnsResult.push({
-        ...item,
-        render: (t: any, row: Record<string, any>, index: number) => {
-          return (
-            <Form.Item name={["list", index, item.dataIndex]} initialValue={t}>
-              <AntdComponent
-                placeholder="请输入"
-                previewMode="form"
-                style={{ width: "100%" }}
-                {...antdProps}
-              />
-            </Form.Item>
-          );
-        },
-      });
+      if (isHidden) {
+        Object.assign(item, { className: styles["antd-editable-hidden"] });
+      }
+
+      if (valueType === "option") {
+        columnsResult.push({
+          ...item,
+          render: (t: any, row: Record<string, any>, rowIndex: number) =>
+            typeof item?.render === "function" ? (
+              item.render(t, row, getKey(rowIndex), {
+                insert,
+                remove,
+                replace,
+              })
+            ) : (
+              <Tooltip title="删除此行">
+                <Button
+                  icon={<DeleteOutlined />}
+                  onClick={() => remove(rowIndex)}
+                />
+              </Tooltip>
+            ),
+        });
+      } else {
+        columnsResult.push({
+          ...item,
+          render: (t: any, row: Record<string, any>, rowIndex: number) => {
+            let formItemPropsResult = {};
+            if (typeof formItemProps === "function") {
+              formItemPropsResult = formItemProps(form, {
+                rowIndex,
+                colIndex,
+                name: [listName, getKey(rowIndex), item.dataIndex],
+                dataIndex: item.dataIndex,
+                listName,
+              });
+            } else if (typeof formItemProps === "object") {
+              formItemPropsResult = formItemProps;
+            }
+            return (
+              <Form.Item
+                initialValue={t}
+                {...formItemPropsResult}
+                name={[listName, getKey(rowIndex), item.dataIndex]}
+              >
+                <AntdComponent
+                  placeholder="请填选"
+                  previewMode={isEditable ? "form" : "text"}
+                  style={{ width: "100%" }}
+                  {...antdProps}
+                />
+              </Form.Item>
+            );
+          },
+        });
+      }
     });
     return columnsResult;
   }, [columns]);
+
   return (
-    <Form>
+    <Form form={form}>
       <PreviewText
         previewMode="text"
         previewPlaceholder="-"
         previewClassName={styles["antd-editable-text"]}
       >
         <Table
-          {...restProps}
           className={styles["antd-editable-table"]}
+          pagination={false}
+          {...restProps}
+          rowKey={(r: any) => Math.random().toString(36).substring(2)}
+          dataSource={list}
           columns={getColumns}
         />
+        {showAddButton && (
+          <Button
+            style={{ marginTop: 8 }}
+            block
+            type="dashed"
+            onClick={() => push({})}
+          >
+            + 添加一行数据
+          </Button>
+        )}
       </PreviewText>
     </Form>
   );
